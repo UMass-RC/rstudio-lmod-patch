b9c01c52465b37003bcbe7d7f1cae29be1c0f192
simon <simonleary@umass.edu>
2022-11-15 18:16:15 -0500

    error handling

--- unity_r_module.R
+++ unity_r_module.R
@@ -1,17 +1,16 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
 unity_r_module <- function(modulename){
-  command = paste(". /etc/profile; module load microarch/x86_64; unity-module-r-libpaths r-", modulename, sep='')
-  commandOutput = system(command, intern=TRUE)
-  newLibPaths = strsplit(commandOutput, ':')[[1]]
-  .libPaths(newLibPaths)
+    # disable warnings
+    beforeWarnOpt = getOption("warn")
+    options(warn=-1)
+
+    command = paste(". /etc/profile; module load microarch/x86_64; unity-module-r-libpaths", modulename)
+    commandOutput = system(command, intern=TRUE)
+    # status attribute is null if command was a success
+    if (is.null(attr(commandOutput, "status"))){
+        newLibPaths = strsplit(commandOutput, ':')[[1]]
+        .libPaths(newLibPaths)
+    }
+
+    # re-enable warnings
+    options(warn=beforeWarnOpt)
 }
