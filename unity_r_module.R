b340a944286f44aaa8e8208388927ddddbba4394
simonleary-umass-edu <71396965+simonleary-umass-edu@users.noreply.github.com>
2022-11-14 21:55:18 -0500

    Update unity_r_module.R

--- unity_r_module.R
+++ unity_r_module.R
@@ -1,14 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
 unity_r_module <- function(modulename){
   command = paste(". /etc/profile; module load microarch/x86_64; unity-module-r-libpaths r-", modulename, sep='')
   commandOutput = system(command, intern=TRUE)
